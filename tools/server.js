2833898d1eb447f1b70f07098bc65af41771486d
dimon85 <glimor800@gmail.com>
2018-03-19 15:31:41 +0200

    add fonts

--- tools/server.js
+++ tools/server.js
@@ -7,6 +7,14 @@ var customPort = 3333;
 var app = express();
 var compiler = webpack(config);
 
+var allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+};
+
 //
 // Register Webpack middleware, hot-reload
 // -----------------------------------------------------------------------------
@@ -19,7 +27,9 @@ app.use(require('webpack-hot-middleware')(compiler));
 //
 // Register Node.js middleware
 // -----------------------------------------------------------------------------
-app.use(express.static(path.join(__dirname, '/assets')));
+app.use('/', express.static(path.join(__dirname, '../assets')));
+app.use('/fonts',express.static(path.join(__dirname, '../assets/fonts')));
+app.use(allowCrossDomain);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
